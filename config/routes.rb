e318479eee6773374361300e5801af3d8289a22d
Kit <dev_kit@icloud.com>
2015-06-03 23:50:35 -0600

    Add an add to cart button and route

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   resources :items,  only: [:index, :show]
   resources :orders, only: [:index, :show]
   resources :users,  only: [:index, :show]
+  resources :cart_items
 end
