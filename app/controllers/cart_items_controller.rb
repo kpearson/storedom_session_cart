459346e1e1c4d7ec6d81b2c6869134fa522d1c58
Kit <dev_kit@icloud.com>
2015-06-04 00:28:15 -0600

    Add cart button to item show page

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,7 +1,9 @@
 class CartItemsController < ApplicationController
+  before_action :load_cart
+
   def create
     item = Item.find(params[:item_id])
-    cart.add_item(item)
-    redirect_to items_path(item)
+    @cart.add_item(item)
+    redirect_to item_path(item)
   end
 end
